5bd8f5622665cfc791255b8c9cd44103ace094ae
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-16 21:46:15 -0300

    deletado mainpage

--- AdCars/App.xaml.cs
+++ AdCars/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdCars.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace AdCars
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginView());
         }
 
         protected override void OnStart()
