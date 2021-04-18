179c77c024cecc26916e7e314a9b28709249c686
Isabel Santos <isabel.santos@worthix.com>
2021-04-18 17:27:39 -0300

    Name register view changed

--- AdCars/Views/RegisterView.xaml.cs
+++ AdCars/Views/RegisterView.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace AdCars.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page1 : ContentPage
+    public partial class RegisterView : ContentPage
     {
-        public Page1()
+        public RegisterView()
         {
             InitializeComponent();
         }
