76a0449b3269632c42d52d67a1a3190c01452ff5
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-17 22:34:31 -0300

    criado a classe para loginviewmodel

---
+++ AdCars/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdCars.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+
+        }
+    }
+}
