1a7490787e13b94b7387f7fa9e76522fdd3d7878
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-17 23:13:57 -0300

    add model Registro

---
+++ AdCars/Models/RegistroModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdCars.Models
+{
+    public class RegistroModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
