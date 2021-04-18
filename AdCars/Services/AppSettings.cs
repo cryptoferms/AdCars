0feb821702fe5630d6f99b2270ba51561272532a
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-18 16:48:19 -0300

    nova atualizacao loginview

--- AdCars/Services/AppSettings.cs
+++ AdCars/Services/AppSettings.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace AdCars.Services
 {
+    //Classe responsável para servir como um helper irá receber a string da nossa URL para testes
     public static class AppSettings
     {
         public static string ApiUrl = "";
