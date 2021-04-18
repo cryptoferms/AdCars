e331bdcdba998a66f5e9f83231d605ab1b3f56ff
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-17 23:14:18 -0300

    add model trocar telefone

---
+++ AdCars/Models/TrocarTelefoneModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdCars.Models
+{
+    public class TrocarTelefoneModel
+    {
+        public string Telefone { get; set; }
+    }
+}
