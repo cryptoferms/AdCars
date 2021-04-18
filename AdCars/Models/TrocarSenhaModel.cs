1c1aabea2549e0725af767e163c2c5409b635416
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-17 23:14:09 -0300

    Add model trocar senha

---
+++ AdCars/Models/TrocarSenhaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdCars.Models
+{
+    public class TrocarSenhaModel
+    {
+        public string SenhaAntiga { get; set; }
+        public string NovaSenha { get; set; }
+        public string ConfirmarSenha { get; set; }
+    }
+}
