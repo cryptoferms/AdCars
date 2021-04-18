649b2af303abe3358b2dce25e85923514350091d
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-17 23:13:48 -0300

    add model login

---
+++ AdCars/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdCars.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
