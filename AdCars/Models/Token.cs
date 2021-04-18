af6dd6fa31bf7da9f345a7335aa675a3ea3ec785
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-18 14:51:45 -0300

    criado a classe para receber a info do Token

---
+++ AdCars/Models/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdCars.Models
+{
+    public class Token
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string token_type { get; set; }
+        public int creation_Time { get; set; }
+        public int expiration_Time { get; set; }
+        public int user_id { get; set; }
+        public string user_nome { get; set; }
+        public string user_email { get; set; }
+    }
+}
