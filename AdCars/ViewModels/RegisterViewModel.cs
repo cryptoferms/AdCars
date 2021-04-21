d1024148622f7a51df1d61ee4e552f68969b6edf
Isabel Santos <isabel.santos@worthix.com>
2021-04-21 20:10:16 -0300

    Tela de register view alinhada

--- AdCars/ViewModels/RegisterViewModel.cs
+++ AdCars/ViewModels/RegisterViewModel.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using AdCars.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace AdCars.ViewModels
 {
     public class RegisterViewModel : BaseViewModel
     {
+        public Command LoginCommand { get; set; }
 
+        public RegisterViewModel()
+        {  
+            LoginCommand = new Command(async () => await LoginCommandAsync());
+        }
+        private async Task LoginCommandAsync()
+        {
+            Application.Current.MainPage = new NavigationPage(new LoginView());
+        }
     }
 }
