fbb1b2dab0c04affe824980ff15a9c244cb6ccc1
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-21 13:11:30 -0300

    metodo para logar no app atualizado

--- AdCars/ViewModels/LoginViewModel.cs
+++ AdCars/ViewModels/LoginViewModel.cs
@@ -1,4 +1,6 @@
-﻿using AdCars.Views;
+﻿using Acr.UserDialogs;
+using AdCars.Services;
+using AdCars.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -28,6 +30,13 @@ namespace AdCars.ViewModels
             get { return _isBusy; }
             set { _isBusy = value; OnPropertyChanged(); }
         }
+        private bool _Result;
+        public bool Result
+        {
+            get { return _Result; }
+            set { _Result = value; OnPropertyChanged(); }
+        }
+
         #endregion
         public Command LoginCommand { get; set; }
         public Command RegistroCommand { get; set; }
@@ -40,15 +49,35 @@ namespace AdCars.ViewModels
 
         private async Task RegisterCommandAsync()
         {
-           Application.Current.MainPage = new NavigationPage(new RegisterView());
+            Application.Current.MainPage = new NavigationPage(new RegisterView());
         }
-
-
         //MÉTODO PARA REALIZAR A LÓGICA DE LOGIN... AINDA NÃO IMPLEMENTADO
         private async Task LoginCommandAsync()
         {
             if (isBusy)
                 return;
+            try
+            {
+                using (UserDialogs.Instance.Loading(title: "Carregando..."))
+                {
+                    isBusy = true;
+                    Result = await ApiService.Login(Email, Senha);
+                    if (Result)
+                    {
+                        //lógica para navegar para a página inicial do App
+                    }
+                    else
+                        await Application.Current.MainPage.DisplayAlert("AVISO", "Não foi possível efetuar o login", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("ERRO", $"O seguinte erro ocorreu: {ex.Message}", "OK");
+            }
+            finally
+            {
+                isBusy = false;
+            }
         }
     }
 }
