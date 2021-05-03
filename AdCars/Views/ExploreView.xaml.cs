a395bfff3d26c09be5bc1988f6144ea61cb391a5
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-03 18:54:23 -0300

    Login & exploreview changes

--- AdCars/Views/ExploreView.xaml.cs
+++ AdCars/Views/ExploreView.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,12 @@ namespace AdCars.Views
         public ExploreView()
         {
             InitializeComponent();
+            nome.Text = Preferences.Get("userInfo", string.Empty);
+        }
+
+        private void CvVehicles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
