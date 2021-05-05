26d9d6d20aa9b6bc9539dea15faa4fd4e36e3a74
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-05 20:28:43 -0300

    nuget package para camera

--- AdCars/Views/MyProfileView.xaml.cs
+++ AdCars/Views/MyProfileView.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace AdCars.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MyInfo : ContentPage
+    public partial class MyProfileView : ContentPage
     {
-        public MyInfo()
+        public MyProfileView()
         {
             InitializeComponent();
         }
