79ea0916564e95ec1f749fdc493131cc732e7e59
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-06 12:13:00 -0300

    novos ajustes na profileview

--- AdCars/ViewModels/MyProfileViewModel.cs
+++ AdCars/ViewModels/MyProfileViewModel.cs
@@ -1,11 +1,68 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace AdCars.ViewModels
 {
     public class MyProfileViewModel : BaseViewModel
     {
 
+        private string _Email;
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; OnPropertyChanged(); }
+        }
+        private string _Image;
+        public string Image
+        {
+            get { return _Image; }
+            set { _Image = value; OnPropertyChanged(); }
+        }
+        private string _Nome;
+        public string Nome
+        {
+            get { return _Nome; }
+            set { _Nome = value; OnPropertyChanged(); }
+        }
+
+
+        //commandos para metodos
+        public Command UploadImageCommand { get; set; }
+
+        public MyProfileViewModel()
+        {
+            UploadImageCommand = new Command(async () => await UploadImageAsync());
+        }
+
+        private async Task UploadImageAsync()
+        {
+            PegarImagemGaleria();
+        }
+
+        private async void PegarImagemGaleria()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await Application.Current.MainPage.DisplayAlert("ERRO", "Desculpe, seu dispositivo não suporta essa funcionalidade ", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            Image = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+        }
     }
 }
