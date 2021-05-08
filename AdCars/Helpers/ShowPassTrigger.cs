72b42c4dcf5bf679832592a6d04ae487971b230c
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-08 14:37:12 -0300

    criado trigger para o botao showpassword

---
+++ AdCars/Helpers/ShowPassTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AdCars.Helpers
+{
+    public class ShowPassTrigger : TriggerAction<ImageButton>, INotifyPropertyChanged
+    {
+        public string ShowIcon { get; set; }
+        public string HideIcon { get; set; }
+
+        private bool _hidePassword = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool HidePassword
+        {
+            set
+            {
+                if (_hidePassword != value)
+                {
+                    _hidePassword = value;
+
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HidePassword)));
+                }
+            }
+            get => _hidePassword;
+        }
+        protected override void Invoke(ImageButton sender)
+        {
+            sender.Source = HidePassword ? ShowIcon : HideIcon;
+            HidePassword = !HidePassword;
+        }
+    }
+}
