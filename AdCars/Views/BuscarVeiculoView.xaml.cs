05711045b5ffb8610d92ab33decf8b5f4e27023a
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-13 12:20:02 -0300

    Search page done

--- AdCars/Views/BuscarVeiculoView.xaml.cs
+++ AdCars/Views/BuscarVeiculoView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AdCars.Models;
+using AdCars.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,24 @@ namespace AdCars.Views
         public BuscarVeiculoView()
         {
             InitializeComponent();
+        }
+
+        private async void SearchBarVehicle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var veiculos = await ApiService.BuscarVeiculos(e.NewTextValue);
+           
+            LvSearch.ItemsSource = veiculos;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            SearchBarVehicle.Focus();
+        }
+
+        private void LvSearch_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var veiculoSelecionado = e.SelectedItem as BuscarVeiculo;
+            Navigation.PushModalAsync(new DetalheVeiculoView(veiculoSelecionado.id));
         }
     }
 }
