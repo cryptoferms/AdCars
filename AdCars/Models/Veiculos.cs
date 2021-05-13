358bfe963900b224e3c0aa9c5af5b9dcb890801b
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-13 14:19:24 -0300

    pagina sellview e novas props para a classe veiculo

--- AdCars/Models/Veiculos.cs
+++ AdCars/Models/Veiculos.cs
@@ -11,6 +11,12 @@ namespace AdCars.Models
         public int CategoriaId { get; set; }
         public string Cor { get; set; }
         public string Fabricante { get; set; }
+        public int Ano { get; set; }
+        public string Cambio { get; set; }
+        public string Direcao { get; set; }
+        public string Portas { get; set; }
+        public string Quilometragem { get; set; }
+        public string Combustivel { get; set; }
         public string Condicao { get; set; }
         public string motor { get; set; }
         public int Preco { get; set; }
