7d4adfb34e195c0d9076ec5da5aea318fb93dc98
fernando Schilipack <nando_fernendo@hotmail.com>
2021-05-31 10:06:01 -0300

    add data para novosrecomendados

--- AdCars/Models/NovosRecomendadosAd.cs
+++ AdCars/Models/NovosRecomendadosAd.cs
@@ -11,8 +11,10 @@ namespace AdCars.Models
         public int preco { get; set; }
         public string modelo { get; set; }
         public string fabricante { get; set; }
+        public DateTime data { get; set; }
         public bool isDestaque { get; set; }
         public string imageUrl { get; set; }
+        public string AdDataPostagem => data.ToString("y");
         public string FullImageUrl => $"https://veiculosapi.conveyor.cloud/{imageUrl}"; 
     }
 }
