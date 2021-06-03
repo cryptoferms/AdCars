bcae4e7f6a834dba4df39bf7a745664f381b1daf
Isabel Santos <isabel.santos@worthix.com>
2021-06-03 19:33:36 -0300

    Custom editor renderer

--- AdCars.iOS/CustomEditor.cs
+++ AdCars.iOS/CustomEditor.cs
@@ -1,23 +1,23 @@
 ﻿using AdCars;
-using AdCars.iOS; 
+using AdCars.iOS;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
-[assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRenderer))]
+[assembly: ExportRenderer(typeof(CustomEditor), typeof(CustomEditorRenderer))]
 
 namespace AdCars.iOS
 {
-    public class CustomPickerRenderer : PickerRenderer
+    public class CustomEditorRenderer : EditorRenderer
     {
-        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
         {
             base.OnElementChanged(e);
 
             if (Control != null)
             {
                 Control.BackgroundColor = UIColor.FromRGB(204, 153, 255);
-                Control.BorderStyle = UITextBorderStyle.Line;
+                // Control.BorderStyle = UITextBorderStyle.Line;
             }
         }
     }
